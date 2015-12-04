1dfeb3f99e1e1eb525a442e300a1076ba35a99fc
johndays <apollo.13@live.com.pt>
2015-12-04 22:30:45 +0000

    attempt socket io connection with node serve

--- www/js/controllers/LoginCtrl.js
+++ www/js/controllers/LoginCtrl.js
@@ -1,16 +1,26 @@
 'use strict';
 
 angular.module('lingu')
-  .controller('LoginCtrl',
+    .controller('LoginCtrl',
     function ($scope, $rootScope) {
+        var socket;
+        $scope.userInfo = {
+            username: "",
+            password: ""
+        };
 
-      $scope.userInfo = {
-        username: "",
-        password: ""
-      };
+        $scope.submitLoginRequest = function () {
+            socket = io('ws://172.18.135.193:8080');
 
-      $scope.submitLoginRequest = function () {
-
-      };
+            socket.on('connect', function () {
+                console.log("connected to server");
+                socket.emit('message', JSON.stringify(
+                    {
+                        action: 2,
+                        username: $scope.userInfo.username,
+                        password: $scope.userInfo.password
+                    }));
+            });
+        };
 
     });
